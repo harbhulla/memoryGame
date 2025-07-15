979d3ce433b76cebb9e37b7911c6ec77f370de53
Harsimran <harsimranbhullar01@gmail.com>
2025-07-15 12:11:45 -0700

    Added state to check for duplicate card selections

--- src/components/cardClick.jsx
+++ src/components/cardClick.jsx
@@ -1,11 +1,21 @@
+import { useEffect } from "react";
+import { useState } from "react";
 export default function CardClick({ data }) {
-    const clickCard = () => {
-        console.log("clicked");
-    }
+    const [acquiredCard, setAcquiredCard] = useState([]);
+    const [inArray,setInArray] = useState([]);
+    const clickCard = (pokemon) => {
+        const alreadyExists = acquiredCard.some(item => item === pokemon);
+        if(alreadyExists) {
+            setInArray((prev) => [...prev,false]);
+        } else {
+            setInArray((prev) => [...prev, true]);
+            setAcquiredCard((prev) => [...prev, pokemon]);
+        }
+    };
     return (
       <div className="cardContainer">
         {data.map((pokemon) => (
-          <div key={pokemon.id} className="pokeCard" onClick={clickCard}>
+          <div key={pokemon.id} className="pokeCard" onClick={() => clickCard(pokemon)}>
             <h2>{pokemon.name}</h2>
             <img src={pokemon.sprites.front_default} alt={pokemon.name} />
           </div>

c7a1f87fd1e931587fb48ee4611a10e3cd04465f
Harsimran <harsimranbhullar01@gmail.com>
2025-07-15 15:29:11 -0700

    Added logic to handle win or lose

--- src/components/cardClick.jsx
+++ src/components/cardClick.jsx
@@ -2,25 +2,46 @@ import { useEffect } from "react";
 import { useState } from "react";
 export default function CardClick({ data }) {
     const [acquiredCard, setAcquiredCard] = useState([]);
-    const [inArray,setInArray] = useState([]);
+    const [deck, setDeck] = useState([]);
+    const [count,setCount] = useState(0);
+    
+
+    useEffect(() => {
+        if(data.length > 0) {
+            setDeck(shuffleArray(data))
+        };
+    }, [data]);
     const clickCard = (pokemon) => {
         const alreadyExists = acquiredCard.some(item => item === pokemon);
         if(alreadyExists) {
-            setInArray((prev) => [...prev,false]);
+            setCount(0);
+            setAcquiredCard([]);
+            console.log("You lose")
         } else {
-            setInArray((prev) => [...prev, true]);
+            setCount((prev) => prev + 1);
             setAcquiredCard((prev) => [...prev, pokemon]);
+            setDeck((prev) => shuffleArray(prev));
         }
     };
+    const shuffleArray = (array) => {
+        const arr = [...array];
+        for(let i = arr.length - 1; i > 0; i--) {
+            const j =  Math.floor(Math.random() * (i + 1));
+            [arr[i], arr[j]] = [arr[j], arr[i]];
+        }
+        return arr;
+    }
     return (
       <div className="cardContainer">
-        {data.map((pokemon) => (
+        {deck.map((pokemon) => (
           <div key={pokemon.id} className="pokeCard" onClick={() => clickCard(pokemon)}>
             <h2>{pokemon.name}</h2>
             <img src={pokemon.sprites.front_default} alt={pokemon.name} />
           </div>
-        ))}
-      </div>
+          ))}
+
+          <h2>{count}</h2>
+          </div>
     );
   }
   

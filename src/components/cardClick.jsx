7cb2d7386ed72d7fa39cf23cd720253f3d32b4e2
Harsimran <harsimranbhullar01@gmail.com>
2025-07-15 16:31:24 -0700

    Added CSS

--- src/components/cardClick.jsx
+++ src/components/cardClick.jsx
@@ -4,8 +4,7 @@ export default function CardClick({ data }) {
     const [acquiredCard, setAcquiredCard] = useState([]);
     const [deck, setDeck] = useState([]);
     const [count,setCount] = useState(0);
-    
-
+    const [bestScore,setBestScore] = useState(0);
     useEffect(() => {
         if(data.length > 0) {
             setDeck(shuffleArray(data))
@@ -14,6 +13,7 @@ export default function CardClick({ data }) {
     const clickCard = (pokemon) => {
         const alreadyExists = acquiredCard.some(item => item === pokemon);
         if(alreadyExists) {
+            setBestScore((prev) => Math.max(prev, count + 1)); 
             setCount(0);
             setAcquiredCard([]);
             console.log("You lose")
@@ -32,15 +32,24 @@ export default function CardClick({ data }) {
         return arr;
     }
     return (
-      <div className="cardContainer">
+      <div className="container">
+      
+      <div className="scores">
+      <div className="game">
+        <h2>Memory Game</h2>
+      </div>
+        <h4>Score: {count}</h4>
+          <h4>Best score: {bestScore}</h4>
+          </div>
+          <div className="title">Get points by clicking on an image, but dont click on anymore than once!</div>
+          <div className="cardContainer">
         {deck.map((pokemon) => (
           <div key={pokemon.id} className="pokeCard" onClick={() => clickCard(pokemon)}>
             <h2>{pokemon.name}</h2>
             <img src={pokemon.sprites.front_default} alt={pokemon.name} />
           </div>
           ))}
-
-          <h2>{count}</h2>
+          </div>
           </div>
     );
   }
